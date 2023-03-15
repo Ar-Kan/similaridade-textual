c181e341a1f0bfbb79ef08cd4bbe763ff7f1464d
Ar-Kan <arqui13a@hotmail.com>
2023-03-15 01:55:29 -0300

    Mensagens de estatus durante a execução de testes

--- rubro_negra_teste/rubro_negra_teste.c
+++ rubro_negra_teste/rubro_negra_teste.c
@@ -7,20 +7,24 @@
 
 int executa_testes() {
     printf_verde(". Iniciando testes");
-    printf("\n");
 
+    printf("\n");
+    printf(". Iniciando testes de criacao\n");
     if (executa_testes_de_criacao()) {
         printf_vermelho(". Testes de criacao terminaram com erro");
         return 1;
     }
+    printf(". Testes de criacao executaram com sucesso\n");
 
+    printf("\n");
+    printf(". Iniciando testes de balanceamento\n");
     if (executa_testes_de_balanceamento()) {
         printf_vermelho(". Testes de transformacao terminaram com erro");
         return 1;
     }
+    printf(". Testes de balanceamento executaram com sucess\n");
 
     printf("\n");
     printf_verde(". Testes executaram com sucesso");
-
     return 0;
 }
