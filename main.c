eaf6389d47565f814016e810e8c3011e94a793d2
Ar-Kan <arqui13a@hotmail.com>
2023-03-16 16:56:52 -0300

    Movendo testes da rubro-negra para a pasta de implementação da árvore

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 #include "rubro_negra/rubro_negra.h"
-#include "rubro_negra_teste/rubro_negra_teste.h"
+#include "rubro_negra/testes/rubro_negra_teste.h"
 
 int main(int argc, char *argv[]) {
     setlocale(LC_ALL, "");
