773f5462bac128943fa8f533ca648bb3f8581688
Ar-Kan <arqui13a@hotmail.com>
2023-03-18 16:34:43 -0300

    Computa tempo médio de execução

--- main.c
+++ main.c
@@ -6,45 +6,44 @@
 #include "rubro_negra/testes/rubro_negra_teste.h"
 #include "rubro_negra/fila_rubro_negra.h"
 
-int computa_jaccard(char *caminho_texto_a, char *caminho_texto_b, char *caminho_stopwords) {
+float computa_jaccard(char *texto_a, char *texto_b, char *stopwords, float *tempo_de_execucao) {
 
     char *palavra, linhas[1000];
     char separadores[] = {" ,.&*%\?!;/-'@\"$#=><()][}{:\n\t"};
     FILE *entrada_stream;
 
     clock_t start, end;
-    float tempo_total = 0;
 
     start = clock();
 
     // Stopwords
-    if ((entrada_stream = fopen(caminho_stopwords, "r")) == NULL) {
-        perror(caminho_stopwords);
-        printf("Erro ao abrir o arquivo: \"%s\"\n", caminho_stopwords);
+    if ((entrada_stream = fopen(stopwords, "r")) == NULL) {
+        perror(stopwords);
+        printf("Erro ao abrir o arquivo: \"%s\"\n", stopwords);
         return 1;
     }
-    Nodo *stopwords = NULL;
+    Nodo *arvore_stops = NULL;
     while (fgets(linhas, 1000, entrada_stream)) {
         palavra = strlwr(strtok(linhas, separadores));
         while (palavra != NULL) {
-            inserir(&stopwords, strlwr(palavra));
+            inserir(&arvore_stops, strlwr(palavra));
             palavra = strlwr(strtok(NULL, separadores));
         }
     }
     fclose(entrada_stream);
 
     // texto base
-    if ((entrada_stream = fopen(caminho_texto_a, "r")) == NULL) {
-        perror(caminho_texto_a);
-        printf("Erro ao abrir o arquivo: \"%s\"\n", caminho_texto_a);
+    if ((entrada_stream = fopen(texto_a, "r")) == NULL) {
+        perror(texto_a);
+        printf("Erro ao abrir o arquivo: \"%s\"\n", texto_a);
         return 1;
     }
-    Nodo *texto_a = NULL;
+    Nodo *arvore_a = NULL;
     while (fgets(linhas, 1000, entrada_stream)) {
         palavra = strlwr(strtok(linhas, separadores));
         while (palavra != NULL) {
-            if (!pesquisar(stopwords, palavra)) {
-                inserir(&texto_a, palavra);
+            if (!pesquisar(arvore_stops, palavra)) {
+                inserir(&arvore_a, palavra);
             }
             palavra = strlwr(strtok(NULL, separadores));
         }
@@ -52,17 +51,17 @@ int computa_jaccard(char *caminho_texto_a, char *caminho_texto_b, char *caminho_
     fclose(entrada_stream);
 
     // texto de busca
-    if ((entrada_stream = fopen(caminho_texto_b, "r")) == NULL) {
-        perror(caminho_texto_b);
-        printf("Erro ao abrir o arquivo: \"%s\"\n", caminho_texto_b);
+    if ((entrada_stream = fopen(texto_b, "r")) == NULL) {
+        perror(texto_b);
+        printf("Erro ao abrir o arquivo: \"%s\"\n", texto_b);
         return 1;
     }
-    Nodo *texto_b = NULL;
+    Nodo *arvore_b = NULL;
     while (fgets(linhas, 1000, entrada_stream)) {
         palavra = strlwr(strtok(linhas, separadores));
         while (palavra != NULL) {
-            if (!pesquisar(stopwords, palavra)) {
-                inserir(&texto_b, palavra);
+            if (!pesquisar(arvore_stops, palavra)) {
+                inserir(&arvore_b, palavra);
             }
             palavra = strlwr(strtok(NULL, separadores));
         }
@@ -70,38 +69,36 @@ int computa_jaccard(char *caminho_texto_a, char *caminho_texto_b, char *caminho_
     fclose(entrada_stream);
 
     Fila *fila = NULL;
-    if (tamanho_arvore(texto_a) <= tamanho_arvore(texto_b)) {
-        fila = listar_nodos(texto_a);
+    if (tamanho_arvore(arvore_a) <= tamanho_arvore(arvore_b)) {
+        fila = listar_nodos(arvore_a);
     } else {
-        fila = listar_nodos(texto_b);
+        fila = listar_nodos(arvore_b);
     }
     int palavras_em_comum = 0;
     while (QUANTIDADE_DE_ELEMENTOS_DE(fila)) {
         Nodo *proximo_nodo = remover_da_fila(&fila);
-        if (pesquisar(texto_a, PALAVRA_DE(proximo_nodo))) {
+        if (pesquisar(arvore_a, PALAVRA_DE(proximo_nodo))) {
             palavras_em_comum++;
         }
     }
 
     end = clock();
-    tempo_total = (float) (end - start) / CLOCKS_PER_SEC * 1000;
+    *tempo_de_execucao = ((float) (end - start) / CLOCKS_PER_SEC * 1000);
 
-    int total_de_palavras = (tamanho_arvore(texto_a) + tamanho_arvore(texto_b)) - palavras_em_comum;
+    int total_de_palavras = (tamanho_arvore(arvore_a) + tamanho_arvore(arvore_b)) - palavras_em_comum;
+    float jaccard = palavras_em_comum * 1.0 / total_de_palavras;
 
-
-    printf("- %d stopwords carregadas\n", tamanho_arvore(stopwords));
-    printf("- %d palavras carregadas para o texto: \"%s\"\n", tamanho_arvore(texto_a), caminho_texto_a);
-    printf("- %d palavras carregadas para o texto: \"%s\"\n", tamanho_arvore(texto_b), caminho_texto_b);
-
+#if 0
+    printf("- %d arvore_stops carregadas\n", tamanho_arvore(arvore_stops));
+    printf("- %d palavras carregadas para o texto: \"%s\"\n", tamanho_arvore(arvore_a), texto_a);
+    printf("- %d palavras carregadas para o texto: \"%s\"\n", tamanho_arvore(arvore_b), texto_b);
     printf("- %d palavras em comum\n", palavras_em_comum);
-
     printf("- %d total de palavras\n", total_de_palavras);
-
     printf("Indice de Jacard: %f\n", palavras_em_comum * 1.0 / total_de_palavras);
+    printf("Tempo: %.5f ms\n", *tempo_de_execucao);
+#endif
 
-    printf("Tempo: %.5f ms\n", tempo_total);
-
-    return 0;
+    return jaccard;
 }
 
 
@@ -114,10 +111,32 @@ int main(int argc, char *argv[]) {
 
     if (argc != 4) {
         printf("Numero incorreto de parametros.\n");
-        printf("Para chamar o programa digite: exemplo <arq_base> <arq_busca> <arq_stopwords>\n");
+        printf("Para chamar o programa digite: exemplo <arq_a> <arq_b> <arq_stopwords>\n");
         return 1;
     }
 
-    computa_jaccard(argv[1], argv[2], argv[3]);
+    float tempo_de_execucao = 0;
+    float tempo_total = 0;
+    float jaccard = computa_jaccard(argv[1], argv[2], argv[3], &tempo_de_execucao);
+    tempo_total += tempo_de_execucao;
+    (void) computa_jaccard(argv[1], argv[2], argv[3], &tempo_de_execucao);
+    tempo_total += tempo_de_execucao;
+    (void) computa_jaccard(argv[1], argv[2], argv[3], &tempo_de_execucao);
+    tempo_total += tempo_de_execucao;
+    (void) computa_jaccard(argv[1], argv[2], argv[3], &tempo_de_execucao);
+    tempo_total += tempo_de_execucao;
+    (void) computa_jaccard(argv[1], argv[2], argv[3], &tempo_de_execucao);
+    tempo_total += tempo_de_execucao;
+    (void) computa_jaccard(argv[1], argv[2], argv[3], &tempo_de_execucao);
+    tempo_total += tempo_de_execucao;
+    (void) computa_jaccard(argv[1], argv[2], argv[3], &tempo_de_execucao);
+    tempo_total += tempo_de_execucao;
+    (void) computa_jaccard(argv[1], argv[2], argv[3], &tempo_de_execucao);
+    tempo_total += tempo_de_execucao;
+    (void) computa_jaccard(argv[1], argv[2], argv[3], &tempo_de_execucao);
+    tempo_total += tempo_de_execucao;
+
+    printf("Indice de Jacard: %f\n", jaccard);
+    printf("Tempo medio de execucao: %.5f ms\n", tempo_total / 8.0);
     return 0;
 }
